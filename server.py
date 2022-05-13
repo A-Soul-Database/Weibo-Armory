781237c5d7da14f990e25414c18dda39d5575577
PPPPP <32994395+P-PPPP@users.noreply.github.com>
2022-05-13 22:55:11 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from threading import Thread
+import requests
+import time
+import uvicorn
+app = FastAPI()
+
+Data = {}
+Times = 0
+
+def Count(fn):
+    global Times
+    Times += 1
+    return fn
+
+def update():
+    url = "https://raw.githubusercontent.com/A-Soul-Database/Weibo-Armory/main/words.json"
+    while True:
+        global Data
+        Data = requests.get(url).json()
+        time.sleep(60)
+
+@Count
+@app.get("/")
+def root():
+    global Times
+    Times+=1
+    return {"code":"0","data":Data}
+
+@Count
+@app.get("/stastics")
+def stastics():
+    global Times
+    return {"code":"0","data":Times}
+
+if __name__ == "__main__":
+    t = Thread(target=update)
+    t.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
